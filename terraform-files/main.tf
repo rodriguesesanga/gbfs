9dc6a1b09b750b2e442e8f93a4eae8e6c0d91c57
rodriguesesanga <48534085+rodriguesesanga@users.noreply.github.com>
2023-05-13 00:02:22 +0200

    Update main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -1,23 +1,3 @@
-resource "null_resource" "install_virtualenv_modules" {
-  provisioner "local-exec" {
-    command = "virtualenv"
-    interpreter = ["python", "-m"]
-  }
-}
-
-resource "null_resource" "create_virtualenv_" {
-  provisioner "local-exec" {
-    command = "../venv"
-    interpreter = ["python", "-m", "virtualenv"]
-  }
-}
-
-resource "null_resource" "activate_virtualenv_" {
-  provisioner "local-exec" {
-    command = "source ../venv/Scripts/activate"
-  }
-}
-
 resource "null_resource" "install_requirements_modules" {
   provisioner "local-exec" {
     command = "../requirements.txt"
