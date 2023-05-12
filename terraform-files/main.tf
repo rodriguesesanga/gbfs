6b3d0c187af7e8196c26280a2109785def151637
rodriguesesanga <48534085+rodriguesesanga@users.noreply.github.com>
2023-05-12 23:18:08 +0200

    Update main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -4,12 +4,14 @@ resource "null_resource" "create_virtual_env" {
     interpreter = ["python", "-m", "pip", "install"]
   }
 }
+
 resource "null_resource" "install_requirements" {
   provisioner "local-exec" {
     command = "../requirements.txt"
     interpreter = ["python", "-m", "pip", "install", "-r"]
   }
 }
+
 resource "null_resource" "execfile" {
   provisioner "local-exec" {
     command = "../main.py"
