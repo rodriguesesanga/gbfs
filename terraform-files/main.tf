36e2b76a7e88eed49756c69782ecac2563f9e6cc
rodriguesesanga <48534085+rodriguesesanga@users.noreply.github.com>
2023-05-12 21:47:55 +0200

    Update main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -1,8 +1,8 @@
 resource "null_resource" "requirements" {
   provisioner "local-exec" {
     #command = "echo ${self.private_ip} >> private_ips.txt"
-    command = "pip install ../requirements.txt"
-    interpreter = ["python", "-m"]
+    command = "../requirements.txt"
+    interpreter = ["python", "-m", "pip install"]
   }
 }
 resource "null_resource" "execfile" {
