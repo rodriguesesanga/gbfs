10b54d95461e4f2a3e6943472a25e07144399d7b
rodriguesesanga <48534085+rodriguesesanga@users.noreply.github.com>
2023-05-12 21:18:42 +0200

    Update main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -1,7 +1,7 @@
 resource "null_resource" "execfile" {
   provisioner "local-exec" {
     #command = "echo ${self.private_ip} >> private_ips.txt"
-    command = "main.py"
+    command = "../main.py"
     interpreter = ["python"]
   }
 }
