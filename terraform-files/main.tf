9b1fd45b73ab927039448fdcdf0314f8e4fe2528
rodriguesesanga <48534085+rodriguesesanga@users.noreply.github.com>
2023-05-13 03:07:57 +0200

    Update main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -1,4 +1,4 @@
-resource "null_resource" "install_requirements_ooobax" {
+resource "null_resource" "install_requirements_ooobeax" {
   provisioner "local-exec" {
     command = "../requirements.txt"
     interpreter = ["python3", "-m", "pip", "install", "--user", "-r"]
