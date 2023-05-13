41d2edeabbdc21133d9432e4da45f4293b65d2fa
rodriguesesanga <48534085+rodriguesesanga@users.noreply.github.com>
2023-05-13 23:40:15 +0200

    Update main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -1,5 +1,4 @@
 resource "null_resource" "installing_requirements_gbfs" {
-  schedule              = "* * * * *"
   provisioner "local-exec" {
     command = "../requirements.txt"
     interpreter = ["python", "-m", "pip", "install", "--user", "-r"]
