6c750f65dd35431a4b89874980abb7a246ee3750
rodriguesesanga <48534085+rodriguesesanga@users.noreply.github.com>
2023-05-13 03:04:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 import logging
 import sys
-sys.path.append('/usr/lib/python3.10/site-packages')
-from . import boto3
+#sys.path.append('/usr/lib/python3.10/site-packages')
+from .. import boto3
 from botocore.exceptions import ClientError
 import os
 import csv
