5ef5ef57059ea6c8896e0ece8373a80f5fcd8f64
rodriguesesanga <rodriguesesanga@gmail.com>
2023-05-13 12:49:18 +0200

    updates files

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -1,13 +1,13 @@
 resource "null_resource" "install_requirements_kka" {
   provisioner "local-exec" {
     command = "../requirements.txt"
-    interpreter = ["py.exe", "-m", "pip", "install", "--user", "-r"]
+    interpreter = ["Python", "-m", "pip", "install", "--user", "-r"]
   }
 }
 
 resource "null_resource" "execfile" {
   provisioner "local-exec" {
     command = "../main.py"
-    interpreter = ["py.exe"]
+    interpreter = ["python"]
   }
 }
