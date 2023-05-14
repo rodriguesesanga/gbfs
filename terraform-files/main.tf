ede5e2d840baf229336cad6b9df949680a7a8a18
rodriguesesanga <48534085+rodriguesesanga@users.noreply.github.com>
2023-05-14 19:59:39 +0200

    Update main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -1,36 +1,36 @@
-resource "null_resource" "git_clone_mz" {
+resource "null_resource" "git_clone_maz" {
   provisioner "local-exec" {
     command = "git clone https://github.com/MobilityData/gbfs.git"
-    interpreter = ["/bin/sh"]
+    interpreter = ["/bin/bash"]
   }
 }
 
-resource "null_resource" "list_files_mz" {
+resource "null_resource" "list_files_maz" {
   provisioner "local-exec" {
     command = "ls"
   }
 }
 
-resource "null_resource" "list_aaa_mz" {
+resource "null_resource" "list_aaa_maz" {
   provisioner "local-exec" {
     command = "ls ../"
   }
 }
 
-resource "null_resource" "copy_system_mz" {
+resource "null_resource" "copy_system_maz" {
   provisioner "local-exec" {
     command = "cp gbfs/systems.csv ../"
   }
 }
 
-resource "null_resource" "install_python_requirements_mz" {
+resource "null_resource" "install_python_requirements_maz" {
   provisioner "local-exec" {
     command = "../requirements.txt"
     interpreter = ["python", "-m", "pip", "install", "--user", "-r"]
   }
 }
 
-resource "null_resource" "run_python_file_mz" {
+resource "null_resource" "run_python_file_maz" {
   provisioner "local-exec" {
     command = "../main.py"
     interpreter = ["python"]
