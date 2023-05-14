eca80e712a2342508b717c6eac5d9b27c66a3dcc
rodriguesesanga <48534085+rodriguesesanga@users.noreply.github.com>
2023-05-14 19:18:27 +0200

    Update main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -1,17 +1,28 @@
-resource "null_resource" "git_clone_systems1" {
+resource "null_resource" "git_clone_zb" {
   provisioner "local-exec" {
     command = "git clone https://github.com/MobilityData/gbfs.git"
-    interpreter = ["bash"]
   }
 }
 
-resource "null_resource" "copy_system_in_root1" {
+resource "null_resource" "list_files" {
+  provisioner "local-exec" {
+    command = "ls"
+  }
+}
+
+resource "null_resource" "list_aaa" {
+  provisioner "local-exec" {
+    command = "ls ../"
+  }
+}
+
+resource "null_resource" "copy_system_zb" {
   provisioner "local-exec" {
     command = "cp gbfs/systems.csv ../"
   }
 }
 
-resource "null_resource" "install_python_requirements1" {
+resource "null_resource" "install_python_requirements1zb" {
   provisioner "local-exec" {
     command = "../requirements.txt"
     interpreter = ["python", "-m", "pip", "install", "--user", "-r"]
