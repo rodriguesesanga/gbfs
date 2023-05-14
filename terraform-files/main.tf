f899b3530092ae4f6bee743fa4bd487dbf07c151
rodriguesesanga <48534085+rodriguesesanga@users.noreply.github.com>
2023-05-15 00:10:27 +0200

    Update main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -1,11 +1,11 @@
-resource "null_resource" "install_requirements_gbfs1" {
+resource "null_resource" "install_requirements_gbfs" {
   provisioner "local-exec" {
     command = "../requirements.txt"
     interpreter = ["python", "-m", "pip", "install", "-r"]
   }
 }
 
-resource "null_resource" "run_py_file_gbfs1" {
+resource "null_resource" "run_py_file_gbfs" {
   provisioner "local-exec" {
     command = "../main.py"
     interpreter = ["python"]
